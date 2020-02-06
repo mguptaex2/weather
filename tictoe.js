f07060d3287705bf480f4c6523f2c08558f438ae
Megha Gupta <mgupta@EX2INDIA.LOCAL>
2020-02-06 17:28:24 +0530

    tictactoe

--- tictoe.js
+++ tictoe.js
@@ -1,43 +1,87 @@
-var Tictactoe = /** @class */ (function () {
-    function Tictactoe() {
+var Tictactoe1 = /** @class */ (function () {
+    function Tictactoe1() {
         //  board :any;
-        this.human = '0';
-        this.computer = 'x';
-        this.win = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [1, 4, 8],
-            [2, 4, 6]
-        ];
+        this.human = 0;
+        this.computer = 1;
+        this.turn = 0;
         this.cells = document.querySelectorAll('.block');
         this.board = Array.from(Array(9).keys());
     }
-    Tictactoe.prototype.start = function (i) {
-        console.log(i);
-        //  const cells = document.querySelectorAll('.block');
-        //  this.board = Array.from(Array(9).keys());
-        console.log(this.board);
-        // console.log(cells.length);
-        // (this.board[i]as HTMLElement).innerText = '';
-        this.blockSelected(i, this.human);
-    };
-    Tictactoe.prototype.blockSelected = function (num, player) {
-        this.board[num] = player;
-        console.log(this.board[num]);
-        console.log(this.board[0]);
-        document.getElementById(num).innerText = player;
-        this.winner();
-    };
-    Tictactoe.prototype.winner = function () {
-        if (this.board[0] === 0 && this.board[1] === 0 && this.board[2] === 0) {
-            console.log('winner');
+    Tictactoe1.prototype.start = function (id) {
+        //    console.log(typeof(this.board));
+        this.turn === this.human;
+        if (this.turn === this.human) {
+            this.blockSelected(id, this.human);
+            this.turn = this.computer;
+        }
+        else {
+            this.blockSelected(id, this.computer);
+            this.turn = this.human;
         }
     };
-    return Tictactoe;
+    Tictactoe1.prototype.blockSelected = function (id, player) {
+        var num = +id;
+        this.board[num] = player;
+        // console.log(typeof(num));
+        // console.log(this.board[0]);
+        if (player === 0) {
+            document.getElementById(id).innerText = '0';
+        }
+        else {
+            document.getElementById(id).innerText = 'x';
+        }
+        var win = this.winner(player);
+        if (win === 0 || win === 1) {
+            alert("winner is " + win);
+            // this.replay();
+            // console.log(this.board);
+        }
+        this.isFull();
+    };
+    Tictactoe1.prototype.isFull = function () {
+    };
+    Tictactoe1.prototype.winner = function (player) {
+        var gameWon = null;
+        // console.log(typeof(this.board[0]));
+        if (this.board[0] === player) {
+            if ((this.board[1] === player && this.board[2] === player) || (this.board[3] === player && this.board[6] === player) || (this.board[4] === player && this.board[8] === player)) {
+                gameWon = player;
+                // console.log("winner");
+            }
+        }
+        if (this.board[1] === player) {
+            if ((this.board[4] === player && this.board[7] === player) || (this.board[0] === player && this.board[2] === player)) {
+                gameWon = player;
+            }
+        }
+        if (this.board[2] === player) {
+            if ((this.board[5] === player && this.board[8] === player) || (this.board[0] === player && this.board[1] === player) || (this.board[4] === player && this.board[6] === player)) {
+                // console.log("winner");
+                gameWon = player;
+            }
+        }
+        if (this.board[3] === player) {
+            if ((this.board[4] === player && this.board[5] === player) || (this.board[0] === player && this.board[6] === player)) {
+                // console.log("winner");
+                gameWon = player;
+            }
+        }
+        if (this.board[6] === player) {
+            if ((this.board[7] === player && this.board[8] === player) || (this.board[0] === player && this.board[3] === player) || (this.board[4] === player && this.board[2] === player)) {
+                // console.log("winner");
+                gameWon = player;
+            }
+        }
+        return gameWon;
+    };
+    Tictactoe1.prototype.replay = function () {
+        // this.board = [0,0,0,0,0,0,0,0,0];
+        for (var i = 0; i < 9; i++) {
+            var str = i.toString();
+            this.board[i] = 0;
+            document.getElementById(str).innerHTML = '';
+        }
+    };
+    return Tictactoe1;
 }());
-var a = new Tictactoe();
 // a.start();
--- tictoe.ts
+++ tictoe.ts
@@ -1,52 +1,141 @@
 
 
-class Tictactoe
+class Tictactoe1
 {
     //  board :any;
-     human = '0';
-     computer = 'x';
-     win = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [1,4,8],
-        [2,4,6]
-    ];
+     human = 0;
+     computer = 1;
+     turn:number = 0;
+    
      cells = document.querySelectorAll('.block');
      board = Array.from(Array(9).keys());
 
-    start(i:number)
+    start(id:string)
     {
-      
-    //  const cells = document.querySelectorAll('.block');
-    //  this.board = Array.from(Array(9).keys());
-    console.log(this.board);
-    // console.log(cells.length);
-    // (this.board[i]as HTMLElement).innerText = '';
-    this.blockSelected(i,this.human);
+//    console.log(typeof(this.board));
+    this.turn === this.human;
+    
+    if(this.turn === this.human)
+    {
+    this.blockSelected(id,this.human);
+    this.turn = this.computer;
+    }
+    else
+    { 
+    this.blockSelected(id, this.computer);
+    this.turn = this.human;
+    }
     }
 
-blockSelected(num:any , player:any)
+blockSelected(id:string, player:number)
 {
-    console.log(num);
+    var num:number =+id;
+ 
     this.board[num] = player;
-    console.log(this.board[num]);
-    console.log(this.board[0]);
-    
-   (document.getElementById(num)as HTMLElement).innerText = player;
-   this.winner();
-}
-winner()
-{
-    if(this.board[0] === 0&& this.board[1] === 0 && this.board[2] === 0 )
+    // console.log(typeof(num));
+    // console.log(this.board[0]);
+    if(player === 0)
     {
-        console.log('winner');
+   (document.getElementById(id)as HTMLElement).innerText = '0';
     }
+    else
+    {
+        (document.getElementById(id)as HTMLElement).innerText = 'x';
+    }
+   let win = this.winner(player);
+   
+   if(win === 0||win === 1){
+   alert("winner is " +win);
+    // this.replay();
+    // console.log(this.board);
+    }
+    this.isFull();
     
+        
+    
+   
 }
+isFull()
+{
+    for(var i = 0;i<9;i++)
+    {
+        var str:string = i.toString();
+        
+       if( (document.getElementById(str)as HTMLElement).innerHTML = '0'||document.getElementById(str)as HTMLElement).innerHTML = 'x')
+       {
+           
+       }
+        
+    }
 }
-let a = new Tictactoe();
+winner(player:number)
+{   
+    
+       
+        let gameWon = null;
+        // console.log(typeof(this.board[0]));
+        if(this.board[0] === player)
+        {
+            if((this.board[1] === player && this.board[2] === player)||(this.board[3] === player && this.board[6] === player)||(this.board[4] === player && this.board[8] === player))
+            {
+                gameWon = player;
+                
+                // console.log("winner");
+            }
+           
+        }
+        if(this.board[1] === player)
+        {
+            if((this.board[4] === player && this.board[7] === player)||(this.board[0] === player && this.board[2] === player))
+            {
+                gameWon = player;
+            }
+           
+        }
+        if(this.board[2] === player)
+        {
+            if((this.board[5] === player && this.board[8] === player)||(this.board[0] === player && this.board[1] === player)||(this.board[4] === player && this.board[6] === player))
+            {
+                // console.log("winner");
+                gameWon = player;
+            }
+           
+        }
+        if(this.board[3] === player)
+        {
+            if((this.board[4] === player && this.board[5] === player)||(this.board[0] === player && this.board[6] === player))
+            {
+                // console.log("winner");
+                gameWon = player;
+            }
+           
+        }
+        if(this.board[6] === player)
+        {
+            if((this.board[7] === player && this.board[8] === player)||(this.board[0] === player && this.board[3] === player)||(this.board[4] === player && this.board[2] === player))
+            {
+                // console.log("winner");
+                gameWon = player;
+            }
+           
+        }
+        
+       
+        return gameWon;
+    }
+
+    replay()
+    {
+        // this.board = [0,0,0,0,0,0,0,0,0];
+        for(var i = 0;i<9;i++)
+        {
+            var str:string = i.toString();
+            this.board[i] = 0;
+            (document.getElementById(str)as HTMLElement).innerHTML = '';
+            
+        }
+    }
+
+}
+
 // a.start();
